768c058f16ed767a09200aac2f5a6fe7eaf57c04
Moritz <mosum@google.com>
2025-05-14 10:24:56 +0200

    Revert "Debug more and mroe"
    This reverts commit a603f3c53be59714df10c54881951c2a1581713f.

--- packages/flutter_tools/lib/src/dart/package_map.dart
+++ packages/flutter_tools/lib/src/dart/package_map.dart
@@ -33,9 +33,6 @@ File? findPackageConfigFile(Directory dir) {
         .childDirectory('.dart_tool')
         .childFile('package_config.json');
     if (candidatePackageConfigFile.existsSync()) {
-      stderr.writeln(
-        'package_config.json found at $candidatePackageConfigFile! - ${StackTrace.current}',
-      );
       return candidatePackageConfigFile;
     }
     final Directory parentDir = candidateDir.parent;
